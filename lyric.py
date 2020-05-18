89fb66ca54750d21eb9b945cbe6f4c87c1d314bf
YuXiang <richardlin0212@gmail.com>
2020-05-18 16:05:48 +0800

    Add input for control

--- lyric.py
+++ lyric.py
@@ -1,4 +1,5 @@
 import os
+import docx
 #import mp3play
 
 #def playmusic(path):
@@ -9,16 +10,32 @@ def main():
     lyrics = []
     mp3s = []
     dirs = []
-    for f in os.listdir('./'):
-        #if os.path.isdir(f):
-        #    dirs.append(f)
+    kinds = []
+
+    # get kinds
+    for f in os.listdir('./mp3/'):
+        if os.path.isdir(f):
+            kinds.append(f)
+
+    print(f'輸入想要編輯的種類代號')
+    for i,k in zip(range(len(kinds)),kinds):
+        print(f'{i}. {k}')
+    
+    input_kind = input()
+
+    # get lyrics and songs
+    for f in os.listdir('./mp3/'+f'{kinds[input_kind]}'):
         file_extend = os.path.splitext(f)[-1]
-        if file_extend == '.mp3':
+        if file_extend in ['.mp3']:
             mp3s.append(f)
-        elif file_extend == '.txt':
+        elif file_extend == ['.txt', '.doc', '.docx']:
             lyrics.append(f)
 
-    print(mp3s)
-    print(lyrics)
+    print(f'輸入想要編輯的歌曲代號')
+    for i,s in zip(range(len(songs)), kinds):
+        print(f'{i}. {s}')
+    
+
+    
 if __name__ == '__main__':
     main()
